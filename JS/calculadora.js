4e529629923cf127264039d01bb7d04ec40a3c47
Ixabel Justo <miju355@vidalibarraquer.net>
2022-05-31 11:46:50 +0200

    Calculadora sin terminar

---
+++ JS/calculadora.js
@@ -0,0 +1,67 @@
+function uno() {
+    pantalla.innerHTML += "1";
+}
+
+function dos() {
+    pantalla.innerHTML += "2";
+}
+
+function tres() {
+    pantalla.innerHTML += "3";
+}
+
+function cuatro() {
+    pantalla.innerHTML += "4";
+}
+
+function cinco() {
+    pantalla.innerHTML += "5";
+}
+
+function seis() {
+    pantalla.innerHTML += "6";
+}
+
+function siete() {
+    pantalla.innerHTML += "7";
+}
+
+function ocho() {
+    pantalla.innerHTML += "8";
+}
+
+function nueve() {
+    pantalla.innerHTML += "9";
+}
+
+function borrarC() {
+    pantalla.innerHTML = " ";
+}
+
+function borrarCE() {
+    pantalla.innerHTML = " ";
+}
+
+function mas() {
+    var suma1 = document.getElementById("pantalla").value;
+    pantalla.innerHTML = "+";
+}
+
+function menos() {
+    var resta1 = document.getElementById("pantalla").value;
+    pantalla.innerHTML = "-";
+}
+
+function por() {
+    var factor1 = document.getElementById("pantalla").value;
+    pantalla.innerHTML = "*";
+}
+
+function entre() {
+    var dividendo = document.getElementById("pantalla").value;
+    pantalla.innerHTML = "/";
+}
+
+function igual() {
+
+}
