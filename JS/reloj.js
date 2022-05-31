81a96e727963cbd739f5228294b319bc4052d6fd
Ixabel Justo <miju355@vidalibarraquer.net>
2022-05-31 12:26:36 +0200

    Ejercicio 16 reloj (no funciona)

---
+++ JS/reloj.js
@@ -0,0 +1,34 @@
+//función principal del reloj
+function relojDigital() {
+
+    //Obtener la fecha y la hora del sistema
+    var fecha = new Date();
+    var horas = fecha.getHours();
+    var minutos = fecha.getMinutes();
+    var segundos = fecha.getSeconds();
+
+    //Si sólo es un dígito, poner un 0 delante
+    horas = cero(horas);
+    minutos = cero(minutos);
+    segundos = cero(segundos);
+
+    //Mostrar el reloj
+    horasHtml = document.getElementById("horas");
+    minutosHtml = document.getElementById("minutos");
+    segundosHtml = document.getElementById("segundos");
+
+    horasHtml.innerHTML = horas;
+    minutosHtml.innerHTML = minutos;
+    segundosHtml.innerHTML = segundos;
+
+    //Actualizar el reloj por segundo
+    setTimeout(function() { relojDigital() }, 1000);
+}
+
+//Función para poner un 0 delante si sólo tiene un dígito
+function cero(valor) {
+    if (valor < 10) {
+        valor = "0" + valor;
+    }
+    return valor;
+}
